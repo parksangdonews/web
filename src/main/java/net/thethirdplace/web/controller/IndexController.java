1b3583f122cab9037720d58b991715ba63c790ea
lucas.choi <lucas.choi@mcngamehub.com>
2019-03-02 16:45:23 +0900

    파일 MVC 경로 수정

--- src/main/java/net/thethirdplace/web/controller/IndexController.java
+++ src/main/java/net/thethirdplace/web/controller/IndexController.java
@@ -3,6 +3,9 @@ package net.thethirdplace.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * 최초 페이지 컨트롤러
+ */
 @Controller
 public class IndexController {
 
